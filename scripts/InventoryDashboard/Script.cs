39b8d207d3e24d458ce0b9ec97f55a83a7c9bf0e
Daniel Baulig <me@danielbaulig.de>
2023-02-23 13:16:58 -0800

    Add component dashboard and construction targets

--- scripts/InventoryDashboard/Script.cs
+++ scripts/InventoryDashboard/Script.cs
@@ -33,11 +33,12 @@ public sealed class Program : MyGridProgram {
      */
     #region InventoryDashboard
 
-    private List<KeyValuePair<string, int>> textSurfaceIdentifiers =
+    private List<KeyValuePair<string, int>> oreDashboardSurfaceIdentifiers =
+     new List<KeyValuePair<string, int>>() { };
+    private List<KeyValuePair<string, int>> componentDashboardSurfaceIdentifiers =
      new List<KeyValuePair<string, int>>() { };
     // List of subtypes to filter the display for
-    // Only Ore and Ingot types supported
-    private List<string> subtypeFilter = new List<string>{ 
+    private List<string> oreSubtypeFilter = new List<string>{ 
             "Ice",
             "Stone",
             "Iron",
@@ -50,6 +51,38 @@ public sealed class Program : MyGridProgram {
             "Uranium",
             "Platinum",
     };
+    private List<string> componentSubtypeFilter = new List<string>{ 
+        "SteelPlate",
+        "InteriorPlate",
+        "Construction",
+        "Motor",
+        "LargeTube",
+        "SmallTube",
+        "Computer",
+        "Girder",
+        "MetalGrid",
+        "Display",
+        "BulletproofGlass",
+        "RadioCommunication",
+        "Detector",
+        "Medical",
+        "GravityGenerator",
+    };
+
+    private Dictionary<string, string> componentTranslation = new Dictionary<string, string> {
+        { "SteelPlate", "Steel"},
+        { "InteriorPlate", "Interior"},
+        { "Construction", "Constr." },
+        { "BulletproofGlass", "Glass"},
+        { "RadioCommunication", "Radio Com."},
+        { "GravityGenerator", "Gravity"},
+        { "LargeTube", "Large Tube"},
+        { "SmallTube", "Small Tube"},
+        { "MetalGrid", "Metal Grid"},
+    };
+
+    private Dictionary<string, int> componentTargets = new Dictionary<string, int>();
+
     IMyProgrammableBlock self = null;
     public IMyProgrammableBlock Self() {
         if (self != null) {
@@ -121,14 +154,10 @@ public sealed class Program : MyGridProgram {
     private Dictionary<string, MyFixedPoint> inventoryDict = new Dictionary<string, MyFixedPoint>();
     private List<MyInventoryItem> itemList = new List<MyInventoryItem>();
 
-    private IMyTextSurface[] textSurfaces;
+    private List<IMyProductionBlock> assemblers = new List<IMyProductionBlock>();
 
-    private void WriteText(string value, bool append = false)
-    {
-        foreach (IMyTextSurface textSurface in textSurfaces) {
-            textSurface.WriteText(value, append);
-        }
-    }
+    private IMyTextSurface[] oreDashboardSurfaces;
+    private IMyTextSurface[] componentDashboardSurfaces;
 
     private string typePrefix = "MyObjectBuilder_";
     private string GetOreKey(string material) {
@@ -137,6 +166,10 @@ public sealed class Program : MyGridProgram {
 
     private string GetIngotKey(string material) {
         return typePrefix + "Ingot:" + material;
+    }
+
+    private string GetComponentKey(string material) {
+        return typePrefix + "Component:" + material;
     }
 
     private MyFixedPoint GetOreAmount(string material) {
@@ -153,22 +186,22 @@ public sealed class Program : MyGridProgram {
         }
         return 0;
     }
+    private MyFixedPoint GetComponentAmount(string material) {
+        string key = GetComponentKey(material);
+        if (inventoryDict.ContainsKey(key)) {
+            return inventoryDict[key];
+        }
+        return 0;
+    }
 
-    public void Main(string argument, UpdateType updateSource)
-    {
-        textSurfaces = new IMyTextSurface[textSurfaceIdentifiers.Count];
-
-        MyFixedPoint totalVolume = 0;
-        MyFixedPoint usedVolume = 0;
-        int i = 0;
-        foreach(var kv in textSurfaceIdentifiers) {
-            var consoleName = kv.Key;
-            var displayIndex = kv.Value;
+    private IMyTextSurface getSurfaceFromIdentifier(KeyValuePair<string, int> identifier) {
+            var consoleName = identifier.Key;
+            var displayIndex = identifier.Value;
 
             var console = GridTerminalSystem.GetBlockWithName(consoleName);
             if (console == null) {
                 Echo(String.Format("ERROR: Failed to find block {0}", consoleName));
-                return;
+                return null;
             }
 
             IMyTextSurface textSurface = null;
@@ -181,21 +214,50 @@ public sealed class Program : MyGridProgram {
                         displayIndex,
                         consoleName
                     ));
-                    return;
+                    return null;
                 }
                 textSurface = textSurfaceProvider.GetSurface(displayIndex);
             }
 
             if (textSurface == null) {
                 Echo(String.Format("ERROR: Block {0} is not a text surface provider.", consoleName));
-                return;
+                return null;
             }
 
             // Set textSurface to correct ContentType and apply Monospace font
             textSurface.ContentType = ContentType.TEXT_AND_IMAGE;
             textSurface.Font = "Monospace";
 
-            textSurfaces[i++] = textSurface;
+            return textSurface;
+    }
+
+    public void Main(string argument, UpdateType updateSource)
+    {
+        oreDashboardSurfaces = new IMyTextSurface[oreDashboardSurfaceIdentifiers.Count];
+        componentDashboardSurfaces = new IMyTextSurface[componentDashboardSurfaceIdentifiers.Count];
+
+        MyFixedPoint totalVolume = 0;
+        MyFixedPoint usedVolume = 0;
+        int i = 0;
+        foreach(var kv in oreDashboardSurfaceIdentifiers) {
+            var surface = this.getSurfaceFromIdentifier(kv);
+
+            if (surface == null) {
+                return;
+            }
+
+            oreDashboardSurfaces[i++] = surface;
+        }
+        
+        i = 0;
+        foreach(var kv in componentDashboardSurfaceIdentifiers) {
+            var surface = this.getSurfaceFromIdentifier(kv);
+
+            if (surface == null) {
+                return;
+            }
+
+            componentDashboardSurfaces[i++] = surface;
         }
 
         // Get list of containers
@@ -238,7 +300,10 @@ public sealed class Program : MyGridProgram {
                 var itemIdentifier = item.Type.TypeId + ":" + item.Type.SubtypeId;
 
                 // If the subtype isn't one of the ones we are looking for, skip
-                if (!subtypeFilter.Exists((filteredSubtype) => itemIdentifier.Contains(filteredSubtype))) {
+                if (
+                    !oreSubtypeFilter.Exists((filteredSubtype) => itemIdentifier.Contains(filteredSubtype)) &&
+                    !componentSubtypeFilter.Exists((filteredSubtype) => itemIdentifier.Contains(filteredSubtype))
+                ) {
                     continue;
                 }
 
@@ -247,22 +312,112 @@ public sealed class Program : MyGridProgram {
                     inventoryDict[itemIdentifier] = 0;
                 }
 
-                // add amount of this identifier in this invenetory to amount we've already seen
+                // add amount of this identifier in this inventory to amount we've already seen
                 inventoryDict[itemIdentifier] = inventoryDict[itemIdentifier] + item.Amount;
             }
             
         }
 
+        string priorityComponent = "";
+        assemblers.Clear();
+        GridTerminalSystem.GetBlocksOfType<IMyProductionBlock>(assemblers);
+        bool assemblerBusy = assemblers.Exists((IMyProductionBlock block) => !block.IsQueueEmpty);
+        bool assemblerError = false;
+
+        if (assembler != null) {
+            foreach(var component in componentSubtypeFilter) {
+                MyFixedPoint componentCount = inventoryDict.GetValueOrDefault(GetComponentKey(component), 0);
+                int componentTarget = componentTargets.GetValueOrDefault(component, 0);
+                var diff = componentTarget - componentCount;
+                if (diff > 0) {
+                    priorityComponent = component;
+
+                    if (assemblerBusy) {
+                        break;
+                    }
+
+                    string blueprintName = getComponentBlueprintName(component);
+                    MyDefinitionId blueprint = MyDefinitionId.Parse(blueprintName);
+                    if (assembler.CanUseBlueprint(blueprint)) {
+                        assembler.AddQueueItem(blueprint, (MyFixedPoint) Math.Min(diff.ToIntSafe(), 100));
+                        break;
+                    } else {
+                        assemblerError = true;
+                        Echo(String.Format("ERROR: Assembler {0} can't use Blueprint {1}", assembler.CustomName, blueprint));
+                    }
+                }
+            }
+        }
+
+
+        foreach (IMyTextSurface textSurface in oreDashboardSurfaces) {
+            writeOreDashboard(textSurface, totalVolume, usedVolume);
+        }
+        
+        foreach (IMyTextSurface textSurface in componentDashboardSurfaces) {
+            writeComponentDashboard(textSurface, priorityComponent, assemblerError);
+        }
+    }
+
+    private string getComponentBlueprintName(string component) {
+        switch (component) {
+            case "Girder":
+            case "Construction":
+            case "Computer":
+            case "Motor":
+            case "Detector":
+            case "Medical":
+            case "RadioCommunication":
+                component = component + "Component";
+                break;
+        }
+        return "MyObjectBuilder_BlueprintDefinition/" + component;
+    }
+
+    private string getComponentDisplayName(string component) {
+        return componentTranslation.GetValueOrDefault(component, component);
+    }
+
+    private void writeComponentDashboard(IMyTextSurface textSurface, string priorityComponent, bool assemblerError) {
+        string formatString = "{0,-10}{1,7}{3,1}{2,7}\n";
+
+        textSurface.WriteText(String.Format(formatString + "\n", "Component", "Target", "Count",""), false);
+
+        componentSubtypeFilter.Sort((l, r) => {
+            MyFixedPoint lAmount = GetComponentAmount(l);
+            MyFixedPoint rAmount = GetComponentAmount(r);
+
+            return (rAmount - lAmount).ToIntSafe();
+        });
+
+        // for each material in the subtype list
+        foreach(string material in componentSubtypeFilter) {
+            MyFixedPoint components = GetComponentAmount(material);
+            
+            int target = componentTargets.GetValueOrDefault(material, 0);
+            
+            string s = String.Format(
+                formatString, 
+                getComponentDisplayName(material),
+                target > 0 ? formatAmount(target) : "",
+                formatAmount(components),
+                priorityComponent == material ? assemblerError ? "!" : "*" : ""
+            );
+            
+            textSurface.WriteText(s, true);
+        }
+    }
+
+    private void writeOreDashboard(IMyTextSurface textSurface, MyFixedPoint totalVolume, MyFixedPoint usedVolume) {
         string formatString = "{0,-10}{1,7} {2,7}\n";
         string lastRowFormatString = "\n{0,20:0%} {1}\n";
 
-
         // Write heading row
-        WriteText(String.Format(formatString + "\n", "Material", "Ingots", "Ore"), false);
+        textSurface.WriteText(String.Format(formatString + "\n", "Material", "Ingots", "Ore"), false);
 
 
         if (enableAutoSort) {
-            subtypeFilter.Sort((l, r) => {
+            oreSubtypeFilter.Sort((l, r) => {
                 MyFixedPoint lAmount = GetOreAmount(l) + GetIngotAmount(l);
                 MyFixedPoint rAmount = GetOreAmount(r) + GetIngotAmount(r);
 
@@ -271,7 +426,7 @@ public sealed class Program : MyGridProgram {
         }
 
         // for each material in the subtype list
-        foreach(string material in subtypeFilter) {
+        foreach(string material in oreSubtypeFilter) {
             // Initialize ore and ingot amount
             MyFixedPoint ore = 0;
             MyFixedPoint ingots = 0;
@@ -279,15 +434,15 @@ public sealed class Program : MyGridProgram {
             ore += GetOreAmount(material);
             ingots += GetIngotAmount(material);
             
-            // Write a line for the material inlcuding the material name and amounts of ingots and ore
+            // Write a line for the material including the material name and amounts of ingots and ore
             string s = String.Format(formatString, material, formatAmount(ingots), formatAmount(ore));
             if (material == "Stone" || material == "Ice") {
                 s = String.Format(formatString, material, "", formatAmount(ore));
             }
-            WriteText(s, true);
+            textSurface.WriteText(s, true);
         }
         
-        WriteText(String.Format(lastRowFormatString, (float)usedVolume/(float)totalVolume,  "Used"), true);
+        textSurface.WriteText(String.Format(lastRowFormatString, (float)usedVolume/(float)totalVolume,  "Used"), true);
     }
 
 
@@ -306,6 +461,8 @@ public sealed class Program : MyGridProgram {
 
     private bool enableLocalGridOnly = false;
     private bool enableAutoSort = false;
+
+    private IMyProductionBlock assembler = null;
     public Program()
     {
         // Run script once every 100 ticks
@@ -316,24 +473,54 @@ public sealed class Program : MyGridProgram {
         enableLocalGridOnly = config.Get("localGridOnly", "false") == "true";
         enableAutoSort = config.Get("autoSort", "false") == "true";
 
-        foreach(Config section in config.sections) {
-            if (section.sectionName != "dashboard") {
-                continue;
-            }
+        string assemblerName = config.Get("assembler", "");
 
-            string blockName = section.Get("block", "");
-            if (blockName == "") {
-                continue;
-            }
-
-            int index = int.Parse(section.Get("surface", "0"));
-
-            textSurfaceIdentifiers.Add(new KeyValuePair<string, int>(blockName, index));
+        if (assemblerName != "") {
+            assembler = GridTerminalSystem.GetBlockWithName(assemblerName) as IMyProductionBlock;
         }
 
-        if (textSurfaceIdentifiers.Count == 0) {
+        foreach(Config section in config.sections) {
+            switch(section.sectionName) {
+                case "dashboard":
+                    string blockName = section.Get("block", "");
+                    if (blockName == "") {
+                        continue;
+                    }
+
+                    string dashboardType = section.Get("type", "ore");
+
+                    int index = int.Parse(section.Get("surface", "0"));
+
+                    KeyValuePair<string, int> kv = new KeyValuePair<string, int>(blockName, index);
+                    switch (dashboardType) {
+                        case "ore":
+                            Echo(String.Format("Ore Dashboard {0}", kv));
+                            oreDashboardSurfaceIdentifiers.Add(kv);
+                            break;
+                        case "components":
+                            Echo(String.Format("Component Dashboard {0}", kv));
+                            componentDashboardSurfaceIdentifiers.Add(kv);
+                            break;
+                        default:
+                            Echo(String.Format("No Type Dashboard {0}", kv));
+                            break;
+                    }
+                    break;
+                
+                case "targets":
+                    foreach(var component in componentSubtypeFilter) {
+                        int target = int.Parse(section.Get(component, "0"));
+                        if (target > 0) {
+                            componentTargets.Add(component, target);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        if (oreDashboardSurfaceIdentifiers.Count == 0) {
             // Keep the default value for backwards compatibility
-            textSurfaceIdentifiers.Add(
+            oreDashboardSurfaceIdentifiers.Add(
                 new KeyValuePair<string, int>(
                     "Inventory Dashboard",
                      0
